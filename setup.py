68186c4101d5aa041077723e000690edc691f4d9
mozan <118672983+imozan@users.noreply.github.com>
2023-07-20 13:48:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import setuptools as s
+from setuptools import setup, find_packages
+
 
 with open("README.me","r",encoding = 'utf8') as dd:
   fg = dd.read()
@@ -11,13 +13,20 @@ s.setup(
   version = "1.0",
   author = "Mozan",
   description = "saveMZ is esay save with json file in python",
-  packages = s.find_packages(),
+  packages = s.find_packages("src"),
+  package_dir = {"" : "src"},
+  url = "https://github.com/imozan/saveMZ.git",
+  project_urls={
+        "Bug Tracker": "https://github.com/imozan/saveMZ/blob/main/src/save_MZ/__init__.py",
+    },
   classifiers = [
     "programming language :: Python :: 3",
     "Operating System :: OS Independent ",
     "License :: OSI Approved :: MIT License "
   ],
   long_description = fg,
-  install_requires = require 
+  long_description_content_type="text/markdown",
+  install_requires = require, 
+  python_requires=">=3.6",
 )
 
