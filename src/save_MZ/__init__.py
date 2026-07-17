0570bfe18f513b62976edd395af2a51b9453a6ac
Julius <118672983+imozan@users.noreply.github.com>
2026-07-17 03:19:22 +0300

    Update __init__.py

--- src/save_MZ/__init__.py
+++ src/save_MZ/__init__.py
@@ -1,21 +1,6 @@
-import json
+from .database import SaveMZ
+from .serilazer import writes, reads
+from .exceptions *
 
-
-def write(file_path,data):
-  with open(file_path,"w",encoding="utf8") as f:
-    json.dump(data,f,indent=2,ensure_ascii=False)
- 
-
-def read(file_path):
-  with open(file_path,"r") as d:
-    data = json.load(d)
-    return data
-
-
-def reads(string):
-    data = json.loads(string)
-    return data
-     
-def writes(data):
-  json.dumps(data,indent=2,ensure_ascii=False)
+__version__ = 2.0
 
